4ac6cfa6d0bbd1f19d5989ff290cb63a4d62b037
Everton Walker <everton.walker@hotmail.com>
2021-05-27 18:39:41 -0300

    init project and create some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ListPokemonsComponent } from './components/list-pokemons/list-pokemons.component';
+import { DetailPokemonComponent } from './components/detail-pokemon/detail-pokemon.component';
+import { NotFoundComponent } from './components/not-found/not-found.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListPokemonsComponent,
+    DetailPokemonComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'pokemon-list', component: ListPokemonsComponent,
+
+      },
+      {
+        path: '/detail-pokemon/:id', component: DetailPokemonComponent,
+      },
+      {
+        path: '', redirectTo: '/pokemon-list', pathMatch: 'full'
+      },
+      { 
+        path: '**', component: NotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
